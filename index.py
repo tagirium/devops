7f70be5c1d8739f4a18f458e55a42472a5a63bdd
tagir <tagir@DESKTOP-QHSACCD>
2021-08-16 13:26:29 +0300

    ready to use

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
-app = Flask(__name__)
+from fastapi.templating import Jinja2Templates
 
-@app.route("/")
-def index():
-    return "Congratulations, it's a web app!"
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/time", response_class=HTMLResponse)
+async def read_item(request: Request):
+	return templates.TemplateResponse("date_time.html", {"request": request})
